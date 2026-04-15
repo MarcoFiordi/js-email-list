fc10f8b03733d9d2f151562766368127f727ec28
Marco Fiordi <marco.fiordi97@gmail.com>
2026-04-15 16:29:19 +0200

    creata funzione generaEmail e reset della lista email

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 'use strict'
+const apiUrl = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
 const emailListElement = document.querySelector('#email-list');
 const buttonGeneraElement = document.querySelector('#generate-btn');
+
+function generaEmail() {
+
+    emailListElement.innerHTML = '';
+
+
+}
